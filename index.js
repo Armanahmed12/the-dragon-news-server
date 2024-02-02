ba162b50dfa8281f3424aa2b5a44beccf69cb4db
Armanahmed12 <mdarmanahmed1232@gmail.com>
2024-02-02 21:52:18 +0600

    there are some changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 
 app.get('/', (req, res) => {
 
-  res.send(" you go to the categories through the categories router.");
+  res.send("You can go to the categories through the categories router.");
 
 })
 app.get('/categories', (req, res) => {

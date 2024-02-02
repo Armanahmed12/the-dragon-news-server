6066bde6f330cd89c06f395047b0dd5aa0f6e276
Armanahmed12 <mdarmanahmed1232@gmail.com>
2024-02-02 19:09:25 +0600

    the-news-dragon-server-data

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000;
+const categories = require('./data/categories.json');
+
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+
+  res.send("categorif you go to the categories route.");
+
+})
+app.get('/categories', (req, res) => {
+
+    res.send(categories);
+  
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

15ed06bfcb0354be1928072718acd0cf3d45630f
Armanahmed12 <mdarmanahmed1232@gmail.com>
2024-02-09 21:51:11 +0600

    vercel configuration has been done

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
-const express = require('express')
-var cors = require('cors')
-const app = express()
+const express = require('express');
+var cors = require('cors');
+const app = express();
 const port = process.env.PORT || 3000;
 const categories = require('./data/categories.json');
-
+const news = require('./data/news.json');
 
 app.use(cors())
-
 app.get('/', (req, res) => {
 
   res.send("You can go to the categories through the categories router.");
 
 })
+
 app.get('/categories', (req, res) => {
 
-    res.send(categories);
-  
-  });
+  res.send(categories);
+
+});
+
+app.get('/allCategoriesNews', (req, res) => {
+
+    res.send(news);
+
+});
+
+app.get('/category/:categoryId', (req, res) => {
+
+  const { params } = req;
+  const { categoryId } = params;
+  if (categoryId == 0 || categoryId == 6) {
+
+    res.send(news);
+  }
+  else {
+
+    const matchedCategoryNews = news.filter(eachNews => eachNews.category_id == categoryId);
+    res.send(matchedCategoryNews);
+
+  }
+
+});
+
+app.get('/newsDetails/:id', (req, res) => {
+
+  const { params } = req;
+  const matchedNews = news.find(eachNews => eachNews._id == params.id);
+  res.send(matchedNews);
+
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
